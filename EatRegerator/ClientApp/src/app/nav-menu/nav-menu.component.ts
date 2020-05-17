f14fd6682418e1e782112b79c25b8d03d7f0c329
alexei619@gmail.com <tokmakov251198>
2020-05-17 15:00:47 +0300

    bootstrap fontawesom home.component

--- EatRegerator/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ EatRegerator/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,6 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav-menu.component.scss']
 })
 export class NavMenuComponent {
+  constructor(private _router: Router) {
+
+  }
   isExpanded = false;
 
   collapse() {
